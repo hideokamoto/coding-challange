a1c76183e6a609ad050b4ed4576c801353d59103
hideokamoto <hide.okamoto@digitalcube.jp>
2018-01-08 23:51:50 +0900

    add google map components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,31 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 import logo from './logo.svg';
 import './App.css';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {lat: 59.95, lng: 30.33},
+    zoom: 11
+  };
+
+  render() {
+    return (
+      <GoogleMapReact
+        defaultCenter={this.props.center}
+        defaultZoom={this.props.zoom}
+      >
+        <AnyReactComponent
+          lat={59.955413}
+          lng={30.337844}
+          text={'Kreyser Avrora'}
+        />
+      </GoogleMapReact>
+    );
+  }
+}
 
 class App extends Component {
   render() {
@@ -10,6 +35,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <div style={{height: '500px'}}>
+          <SimpleMap />
+        </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
