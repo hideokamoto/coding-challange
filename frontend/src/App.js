716ec88fe1902ffd59dd3ff9550a94e0f81ea244
hideokamoto <hide.okamoto@digitalcube.jp>
2018-01-08 23:56:22 +0900

    format by prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import GoogleMapReact from 'google-map-react'
+import logo from './logo.svg'
+import './App.css'
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const AnyReactComponent = ({ text }) => <div>{text}</div>
 
 class SimpleMap extends Component {
   static defaultProps = {
-    center: {lat: 59.95, lng: 30.33},
+    center: { lat: 59.95, lng: 30.33 },
     zoom: 11
-  };
+  }
 
-  render() {
+  render () {
     return (
       <GoogleMapReact
         defaultCenter={this.props.center}
@@ -23,27 +23,27 @@ class SimpleMap extends Component {
           text={'Kreyser Avrora'}
         />
       </GoogleMapReact>
-    );
+    )
   }
 }
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <div style={{height: '500px'}}>
+        <div style={{ height: '500px' }}>
           <SimpleMap />
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
