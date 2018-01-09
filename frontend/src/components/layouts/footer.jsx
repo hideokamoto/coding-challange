0f0b5f214901f736b6268933c9b2700fa73a58a3
hideokamoto <hide.okamoto@digitalcube.jp>
2018-01-09 23:52:42 +0900

    open new tab

--- frontend/src/components/layouts/footer.jsx
+++ frontend/src/components/layouts/footer.jsx
@@ -14,14 +14,22 @@ const AppFooter = props => {
         <ul>
           <li>
             京都市営地下鉄時刻表，京都市，クリエイティブ・コモンズ・ライセンス表示4.0国際<br />
-            <a href="http://creativecommons.org/licenses/by/4.0/deed.ja">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="http://creativecommons.org/licenses/by/4.0/deed.ja"
+            >
               http://creativecommons.org/licenses/by/4.0/deed.ja
             </a>{" "}
             (link is external)
           </li>
           <li>
             京都市営地下鉄各駅位置データ<br />
-            <a href="http://www.odekakemap.com/station/">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="http://www.odekakemap.com/station/"
+            >
               http://www.odekakemap.com/station/
             </a>{" "}
             (link is external)
